e6ba3d4a43136dc75c2cc06a14d9622a5b46c281
yangjinlong <yangjinlong86@gmail.com>
2021-11-15 23:28:50 +0800

    react render HTML

--- 04.react-render-html/my-react-app/src/index.js
+++ 04.react-render-html/my-react-app/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <p>Hello</p>,
-  document.getElementById('root')
+const element = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>Jason</td>
+        </tr>
+        <tr>
+            <td>Funny</td>
+        </tr>
+        <tr>
+            <td>Seven</td>
+        </tr>
+    </table>
 );
+// The root node can be called whatever you like
+ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(element, document.getElementById("test"));
 
